ad827853b118d35ce434178abdf4ae29b1d39946
Cigi <54045350+cigi801@users.noreply.github.com>
2023-10-24 16:03:13 -0600

    Added server.js

---
+++ cit262-stedi-app/server.js
@@ -0,0 +1,11 @@
+const express =require('express');
+
+const port = 3000;
+
+const app = express();
+
+app.get('/', (req,res) => {res.send("Hello Browser")});
+
+app.get('/cigi', (req,res) => {res.send("Hello Cigi")});
+
+app.listen(port, ()=>console.log("Listening"));
